64017e70edc24d8add5891b06b016eed3390bc05
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-06-26 21:06:25 +0500

    starter code ready

---
+++ car/app/page.tsx
@@ -0,0 +1,11 @@
+import {Hero} from '@/components/Index';
+
+
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <Hero/>
+    </main>
+  )
+}
---
+++ car/components/Index.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+import Hero from "./Hero";
+
+export {
+    Hero
+}
